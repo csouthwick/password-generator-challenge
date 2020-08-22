e30894d6e99d66f0c56ccd93c81493f705a7fa78
Chris Southwick <csouthwick@live.com>
2020-08-22 17:42:30 -0400

    Prompt for password length and validate input

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,27 @@
 // Assignment code here
+function generatePassword() {
+  // Prompt for the password length
+  var pwLength = prompt("How long should the password be? Enter a whole number between 8 and 128.");
 
+  //If pwLength is null, keep it as null, else convert to a number. This makes input validation easier.
+  pwLength = (pwLength === null) ? null : Number(pwLength);
+
+  // Test if the input is an integer between 8 and 128 and prompt if it is not
+  while (!Number.isInteger(pwLength) || pwLength < 8 || pwLength > 128) {
+    // Allow users to cancel password creation by pressing the cancel button at any time
+    if (pwLength === null) {
+      alert("Password creation has been canceled");
+      //Return an empty string as no password was created
+      return "";
+    }
+    pwLength = prompt("Invalid input received. Please enter a whole number between 8 and 128 for the password length.");
+
+    //If pwLength is null, keep it as null, else convert to a number. This makes input validation easier.
+    pwLength = (pwLength === null) ? null : Number(pwLength);
+  }
+  console.log(pwLength);
+
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
